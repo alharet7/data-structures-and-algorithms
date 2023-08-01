8fcb1f398c6f4ec1e9313ec2029d68706c3554d7
AlHareth alhyari <hdhareth@gmail.com>
2023-08-01 03:39:34 +0300

    done

---
+++ Code-Challenge16/MaxTree/MaxTree/BinarySearchTree .cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxTree
+{
+    public class BinarySearchTree : BinaryTree
+    {
+        public void Add(int value)
+        {
+            Root = AddNode(Root, value);
+        }
+
+        private Node AddNode(Node node, int value)
+        {
+            if (node == null)
+            {
+                return new Node(value);
+            }
+
+            if (value < node.Value)
+            {
+                node.Left = AddNode(node.Left, value);
+            }
+            else if (value > node.Value)
+            {
+                node.Right = AddNode(node.Right, value);
+            }
+
+            return node;
+        }
+
+        public bool Contains(int value)
+        {
+            return ContainsValue(Root, value);
+        }
+
+        private bool ContainsValue(Node node, int value)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            if (node.Value == value)
+            {
+                return true;
+            }
+            else if (value < node.Value)
+            {
+                return ContainsValue(node.Left, value);
+            }
+            else
+            {
+                return ContainsValue(node.Right, value);
+            }
+        }
+    }
+}
---
+++ Code-Challenge16/MaxTree/MaxTree/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MaxTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.Add(5);
+            binarySearchTree.Add(3);
+            binarySearchTree.Add(7);
+            binarySearchTree.Add(2);
+            binarySearchTree.Add(4);
+            binarySearchTree.Add(6);
+            binarySearchTree.Add(8);
+
+
+
+
+            Console.WriteLine("The Max value is:");
+            Console.WriteLine(binarySearchTree.FindMax());
+
+
+            Console.ReadKey();
+        }
+    }
+}
