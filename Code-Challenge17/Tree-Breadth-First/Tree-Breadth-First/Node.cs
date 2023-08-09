e4cdb7cffc7a9cf8bb095597c30faa2948af4547
AlHareth alhyari <hdhareth@gmail.com>
2023-08-09 07:24:58 +0300

    CC18 Done!

--- Code-Challenge17/Tree-Breadth-First/Tree-Breadth-First/Node.cs
+++ Code-Challenge17/Tree-Breadth-First/Tree-Breadth-First/Node.cs
@@ -10,12 +10,12 @@ namespace Trees
     {
         public Node left { get; set; }
         public Node right { get; set; }
-
         public int value { get; set; }
 
         public Node(int value)
         {
             this.value = value;
+
             left = right = null;
         }
     }
