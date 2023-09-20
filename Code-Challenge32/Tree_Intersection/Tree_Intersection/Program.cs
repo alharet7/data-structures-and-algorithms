2191892a9e75a43350fb5bfecb06e14e786e6039
AlHareth alhyari <hdhareth@gmail.com>
2023-09-21 02:47:15 +0300

    CC 32 Done!

---
+++ Code-Challenge32/Tree_Intersection/Tree_Intersection/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tree_Intersection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create two binary search trees
+            BinarySearchTree tree1 = new BinarySearchTree();
+            BinarySearchTree tree2 = new BinarySearchTree();
+
+            // Add nodes to the trees
+            tree1.Add(5);
+            tree1.Add(3);
+            tree1.Add(8);
+
+            tree2.Add(5);
+            tree2.Add(2);
+            tree2.Add(7);
+
+            // Find the intersection
+            var intersection = TreeIntersection.treeIntersection(tree1, tree2);
+
+            // Display the result
+            Console.WriteLine("Intersection:");
+            foreach (var item in intersection)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
