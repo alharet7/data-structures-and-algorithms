638a6d7d328ea42597f2c91ace16ae56142e4823
AlHareth alhyari <hdhareth@gmail.com>
2023-10-07 06:53:13 +0300

    CC 36 Done!!

---
+++ Code-Challenge36/graph-breadth-first/graph-breadth-first/Graph.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graph_breadth_first
+{
+    public class Graph
+    {
+        public List<Node> Nodes { get; set; }
+
+        public Graph()
+        {
+            Nodes = new List<Node>();
+        }
+
+        public List<Node> BreadthFirst(Node start)
+        {
+            List<Node> visited = new List<Node>();
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                Node current = queue.Dequeue();
+                if (!visited.Contains(current))
+                {
+                    visited.Add(current);
+                    foreach (Node neighbor in current.Neighbors)
+                    {
+                        queue.Enqueue(neighbor);
+                    }
+                }
+            }
+
+            return visited;
+        }
+    }
+}
---
+++ Code-Challenge36/graph-breadth-first/graph-breadth-first/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graph_breadth_first
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public List<Node> Neighbors { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+            Neighbors = new List<Node>();
+        }
+    }
+}
